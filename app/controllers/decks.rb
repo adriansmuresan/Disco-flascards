a014d572e3fff4d78f64ea85fbff38b67b2c3331
Adrian S Muresan & Jesus Roman <pair+adriansmuresan.romanjesus@devbootcamp.com>
2016-08-26 17:03:24 -0500

    got the guess algorithm to work

--- app/controllers/decks.rb
+++ app/controllers/decks.rb
@@ -4,7 +4,13 @@ get '/rounds/:round_id/decks/:deck_id/cards/:card_id' do
 
     #If this is the first card the user is seeing do the sample
     @card_to_show = @round.cards.sample
-    #Otherwise get all the cards in the deck and subtract the ones where they guessed correclty and get the sample of that
+    #Otherwise get all the cards in the deck and subtract the ones where they guessed correctly and get the sample of that
+      #find all of the cards
+      #find all of the cards where the card.guess.result == true for this round id
+      #delete the later from the former
+      #give me a .sample of what's leftover
+
+
     erb :'/cards/show'
 end
 
@@ -12,8 +18,27 @@ end
 post '/rounds/:round_id/decks/:deck_id/cards/:card_id' do
 
     @round = Round.find(params[:round_id])
+    @card = Card.find(params[:card_id])
+
     #Now we're going to create the logic to check the guess of the user
+    if params[:answer] == @card.answer
+        #Check if this card/round combo is already in the guesses table
+        #if it is then update the result
+        #otherwise create a new guess
+        guess_on_table = Guess.find_by(round: @round, card: @card)
 
+      if guess_on_table != nil
+        guess_on_table.result = true
+        guess_on_table.save
+      else
+        guess = Guess.new({ round: @round, card: @card, result: true})
+        guess.save
+      end
 
-    redirect "/rounds/#{@round.id]}/decks/#{@round.deck.id}/cards/#{params[:card_id]}"
+    else
+      guess = Guess.new({ round: @round, card: @card, result: false})
+      guess.save
+    end
+
+    redirect "/rounds/#{@round.id}/decks/#{@round.deck.id}/cards/#{params[:card_id]}"
 end
