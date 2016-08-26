23fca0b2ba34605843f9492e2cfc212bcd125668
Adrian S Muresan & Jesus Roman <pair+adriansmuresan.romanjesus@devbootcamp.com>
2016-08-26 14:51:41 -0500

    redone the associations between round and guesses, droped the database and remigrate

---
+++ app/controllers/rounds.rb
@@ -0,0 +1,9 @@
+get 'rounds/:id/decks/:deck_id/show' do
+
+    round = Round.find(params[:id])
+
+    
+
+  #
+
+end
--- app/models/round.rb
+++ app/models/round.rb
@@ -1,5 +1,6 @@
 class Round < ActiveRecord::Base
   belongs_to :user
   belongs_to :deck
-  has_many :guesses
+  has_many :cards, through: :deck, source: :cards
+  has_many :guesses, through: :cards, source: :guesses
 end
